a594beb7e82e49c5de728c21b694f1a71d3e60bd
MrWoodsman <mikolaj.cwikla12@gmail.com>
2025-10-14 10:47:37 +0200

    move ui to component statistics.jsx

---
+++ src/components/statistics.jsx
@@ -0,0 +1,7 @@
+export const Statistics = () => {
+    return (
+        <div className="main-body h-full flex items-center justify-center text-neutral-400">
+            <p>Tutaj pojawią się statystyki i zadania.</p>
+        </div>
+    )
+}
